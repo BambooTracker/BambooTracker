549490b9bd5a40385b96a4cf9aa7da4517ec3832
rerrahkr <rerrah57@gmail.com>
2018-11-21 00:15:46 +0900

    Fix pitch

--- BambooTracker/pitch_converter.hpp
+++ BambooTracker/pitch_converter.hpp
@@ -13,11 +13,18 @@ public:
 	static uint16_t getPitchSSGSaw(Note note, int octave, int pitch);
 
 private:
-	static const uint16_t centTableFM_[384];
-	static const uint16_t centTableSSGSquare_[384];
-	static const uint16_t centTableSSGTriangle_[384];
-	static const uint16_t centTableSSGSaw_[384];
+	static const uint16_t centTableFM_[3072];
+	static const uint16_t centTableSSGSquare_[3072];
+	static const uint16_t centTableSSGTriangle_[3072];
+	static const uint16_t centTableSSGSaw_[3072];
 
 	PitchConverter();
-	static void calc(int& index, int& octave);
+
+	static int calculateIndex(int octave, Note note, int pitch)
+	{
+		int idx = 384 * octave + static_cast<int>(note) + pitch;
+		if (idx < 0) return 0;
+		else if (idx > 3071) return 3071;
+		else return idx;
+	}
 };
