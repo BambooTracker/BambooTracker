33b45098c69c9aa05c470885041165e9d1f86068
rerrahkr <rerrah57@gmail.com>
2020-08-09 20:29:18 +0900

    Skip unnecessary library copy at deployment for Windows

--- ci/package_windows.sh
+++ ci/package_windows.sh
@@ -2,9 +2,19 @@
 
 set -e
 
+DEPLOY_OPTIONS="--no-quick-import --no-system-d3d-compiler --no-compiler-runtime --no-webkit2 --no-angle --no-svg"
+ADDITIONAL_OPTIONS=("--no-opengl-sw" "--no-virtualkeyboard")
+
+for opt in ${CHECK_OPTIONS[@]}; do
+  if [[ $(./windeployqt.exe -h | grep -e $opt) ]]; then
+    DEPLOY_OPTIONS="$DEPLOY_OPTIONS $opt"
+  fi
+done
+
 cp -a "$(find ../ -name BambooTracker.exe)" ../{img,demos,licenses,specs,skins,*.md,LICENSE} .
-windeployqt BambooTracker.exe -verbose=2
+windeployqt BambooTracker.exe -verbose=2 $DEPLOY_OPTIONS
 mv translations lang
 mv ../BambooTracker/.qm/*.qm lang/
+rm -rf imageformats
 
 exit 0
