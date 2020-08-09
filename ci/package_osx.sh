330df321db842f476a447d15855de210d186897d
rerrahkr <rerrah57@gmail.com>
2020-08-09 23:01:54 +0900

    Deploy qt_** translation files in packaging for macOS

--- ci/package_osx.sh
+++ ci/package_osx.sh
@@ -4,7 +4,13 @@ set -e
 
 cp -a "$(find ../ -name BambooTracker.app)" ../{img,demos,licenses,specs,skins,*.md,LICENSE} .
 macdeployqt BambooTracker.app -verbose=2
-mv ../BambooTracker/.qm/ BambooTracker.app/Contents/Resources/lang
+LANGDIR="BambooTracker.app/Contents/Resources/lang"
+mv ../BambooTracker/.qm/ $LANGDIR
+
+TRDIR="$(dirname $(which lconvert))/translations"
+for lang in $(find $TRDIR/qtbase_*.qm | sed "s/^.*qtbase_\(.*\)\.qm$/\1/"); do
+  lconvert -o "$LANGDIR/qt_$lang.qm" "$TRDIR/qtbase_$lang.qm" "$TRDIR/qtmultimedia_$lang.qm"
+done
 
 rm -rf BambooTracker.app/Contents/Frameworks/{QtPdf.framework,QtQuick.framework,QtVirtualKeyboard.framework,QtQmlModels.framework,QtSvg.framework,QtQml.framework,QtOpenGL.framework,QtMultimediaWidgets.framework}
 rm -rf BambooTracker.app/Contents/PlugIns/{platforminputcontexts,virtualkeyboard,iconengines,imageformats,audio,bearer,mediaservice}
