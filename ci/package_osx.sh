c6dae3ae6ed36864235a023a8d8f200adb388db3
rerrahkr <rerrah57@gmail.com>
2020-08-09 20:48:50 +0900

    Remove unnecessary frameworks and plugins at deployment for macOS

--- ci/package_osx.sh
+++ ci/package_osx.sh
@@ -6,4 +6,7 @@ cp -a "$(find ../ -name BambooTracker.app)" ../{img,demos,licenses,specs,skins,*
 macdeployqt BambooTracker.app -verbose=2
 mv ../BambooTracker/.qm/ BambooTracker.app/Contents/Resources/lang
 
+rm -rf BambooTracker.app/Contents/Frameworks/{QtPdf.framework,QtQuick.framework,QtVirtualKeyboard.framework,QtQmlModels.framework,QtSvg.framework,QtQml.framework,QtOpenGL.framework,QtMultimediaWidgets.framework}
+rm -rf BambooTracker.app/Contents/PlugIns/{platforminputcontexts,virtualkeyboard,iconengines,imageformats,audio,bearer,mediaservice}
+
 exit 0
