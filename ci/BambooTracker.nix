aaaa4b647119fac1e16b1d9cd5fb712d7373a8d0
OPNA2608 <christoph.neidahl@gmail.com>
2020-10-22 10:52:57 +0200

    Blub

--- ci/BambooTracker.nix
+++ ci/BambooTracker.nix
@@ -5,6 +5,7 @@ let
   bambootracker-local = pkgs.bambootracker.overrideAttrs (oldAttrs: {
     version = buildVersion;
     src = ./..;
+    sourceRoot = "BambooTracker";
   });
 in {
   build = bambootracker-local;
