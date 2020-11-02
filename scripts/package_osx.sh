5ba4d5e902e00130d224ba0e6cad8bb7e47ca630
OPNA2608 <christoph.neidahl@gmail.com>
2020-11-02 14:20:18 +0100

    Fix AppVeyor, unify repo-only scripts

--- scripts/package_osx.sh
+++ scripts/package_osx.sh
@@ -7,7 +7,6 @@ macdeployqt BambooTracker.app -verbose=2
 
 LANGDIR="BambooTracker.app/Contents/Resources/lang"
 mv lang $LANGDIR
-ls -ahl "$LANGDIR"
 find "$(dirname $(dirname $(realpath $(which qmake))))/translations" -type f -name 'qtbase_*.qm' -print0 \
 | while IFS= read -r -d '' qtbasetranslation; do
   cp -v "$qtbasetranslation" "$(echo $qtbasetranslation | sed -e 's,.*qtbase_,'"$LANGDIR"'/qt_,g')"
