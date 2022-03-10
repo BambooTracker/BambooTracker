e46bf404a2f5eaac4ef2b9978c508333d4f7ff8e
OPNA2608 <christoph.neidahl@gmail.com>
2022-03-10 11:49:18 +0100

    CMake improvements

--- scripts/build_nixpkgs_local.nix
+++ scripts/build_nixpkgs_local.nix
@@ -7,6 +7,13 @@ let
       src = ./..;
       patches = [];
       preConfigure = "lupdate Project.pro";
+
+      nativeBuildInputs = with pkgs; [ cmake libsForQt5.qttools libsForQt5.wrapQtAppsHook pkg-config ];
+      cmakeFlags = [
+        "-DSYSTEM_RTAUDIO=ON"
+        "-DSYSTEM_RTMIDI=ON"
+      ];
+      postConfigure = ""; #"export VERBOSE=1";
     });
   };
   pkgs = import <nixpkgs> { config = { inherit packageOverrides; }; };
