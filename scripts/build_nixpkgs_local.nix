a2954b46f87c98307f0c94be5c0663dfa88b511d
OPNA2608 <christoph.neidahl@gmail.com>
2022-03-10 12:05:22 +0100

    Remove WIP Nix CMake testing

--- scripts/build_nixpkgs_local.nix
+++ scripts/build_nixpkgs_local.nix
@@ -7,13 +7,6 @@ let
       src = ./..;
       patches = [];
       preConfigure = "lupdate Project.pro";
-
-      nativeBuildInputs = with pkgs; [ cmake libsForQt5.qttools libsForQt5.wrapQtAppsHook pkg-config ];
-      cmakeFlags = [
-        "-DSYSTEM_RTAUDIO=ON"
-        "-DSYSTEM_RTMIDI=ON"
-      ];
-      postConfigure = ""; #"export VERBOSE=1";
     });
   };
   pkgs = import <nixpkgs> { config = { inherit packageOverrides; }; };
