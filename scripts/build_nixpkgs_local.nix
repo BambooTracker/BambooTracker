cadc398575339625965e8f6775d3363988c93753
OPNA2608 <christoph.neidahl@gmail.com>
2023-07-31 13:13:21 +0200

    Fix Nixpkgs build failure
    by building an updated RtAudio while the stable version we use doesn't have it yet.
    Also bump Nixpkgs to latest stable version.

--- scripts/build_nixpkgs_local.nix
+++ scripts/build_nixpkgs_local.nix
@@ -2,7 +2,18 @@
 
 let
   packageOverrides = pkgs: {
-    bambootracker = pkgs.bambootracker.overrideAttrs (oldAttrs: {
+    bambootracker = (pkgs.bambootracker.override {
+			# RtAudio in stable Nixpkgs still on 5.0.0
+      rtaudio = pkgs.rtaudio.overrideAttrs (oa: rec {
+        version = "6.0.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "thestk";
+          repo = "rtaudio";
+          rev = version;
+          hash = "sha256-/pbtezm3Ceo9WsMI1REJ9y72aZck55tRzrsifNeKrXA=";
+        };
+      });
+    }).overrideAttrs (oldAttrs: {
       version = buildVersion;
       src = ./..;
       patches = [];
