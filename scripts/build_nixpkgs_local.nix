603dd0703b334b6d934d0c76ba9efee88c1c4a23
OPNA2608 <opna2608@protonmail.com>
2026-02-08 12:22:24 +0100

    ci: Switch nixpkgs channel to rolling, build Qt6 derivation

--- scripts/build_nixpkgs_local.nix
+++ scripts/build_nixpkgs_local.nix
@@ -2,22 +2,13 @@
 
 let
   packageOverrides = pkgs: {
-    bambootracker = (pkgs.bambootracker.override {
-			# RtAudio in stable Nixpkgs still on 5.0.0
-      rtaudio = pkgs.rtaudio.overrideAttrs (oa: rec {
-        version = "6.0.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "thestk";
-          repo = "rtaudio";
-          rev = version;
-          hash = "sha256-/pbtezm3Ceo9WsMI1REJ9y72aZck55tRzrsifNeKrXA=";
-        };
-      });
-    }).overrideAttrs (oldAttrs: {
+    bambootracker-qt6 = pkgs.bambootracker-qt6.overrideAttrs (oldAttrs: {
       version = buildVersion;
       src = ./..;
       patches = [];
-      preConfigure = "lupdate Project.pro";
+      preConfigure = (oldAttrs.preConfigure or "") + ''
+        lupdate Project.pro
+      '';
     });
   };
   pkgs = import <nixpkgs> { config = { inherit packageOverrides; }; };
@@ -26,7 +17,7 @@ let
     patches = (oa.patches or []) ++ [
       (pkgs.fetchpatch {
         name = "0001-nix-bundle-nix-user-chroot-Link-libgcc_s.so.1.patch";
-        url = "https://github.com/matthewbauer/nix-bundle/pull/98/commits/c047b32dc4d004189c3564eae458aecc5d28f25d.patch";
+        url = "https://github.com/nix-community/nix-bundle/commit/c047b32dc4d004189c3564eae458aecc5d28f25d.patch";
         hash = "sha256-VR45xP7rw3GzIrkznHLPPf9OIVZnlnezUjCKhfQJ47s=";
       })
     ];
@@ -37,9 +28,9 @@ let
     '';
   });
 in {
-  build = pkgs.bambootracker;
+  build = pkgs.bambootracker-qt6;
   bundle = (import "${nix-bundle-patched}/share/nix-bundle/default.nix" {}).nix-bootstrap {
-    target = pkgs.bambootracker;
+    target = pkgs.bambootracker-qt6;
     run = "/bin/BambooTracker";
   };
 }
