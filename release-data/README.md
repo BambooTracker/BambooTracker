3eeeafeff1f68e3b6707558a7e2029dbca23b8d7
OPNA2608 <opna2608@protonmail.com>
2026-07-15 17:20:53 +0200

    ci: Switch nixpkgs channel to rolling, build Qt6 derivation, drop Linux release
    - nixpkgs-unstable
      Can't keep up with the channel pinning, better than pulling in insecure stuff.
    - Qt6
      Qt5 is on its way out.
    - No more Linux releases
      It was always just a huge hassle and caused issues. Wait for your distro to update their package, or learn to build
      from source please.
      Linux-specific readme goes along with it.

--- release-data/README.md
+++ release-data/README.md
@@ -1,1 +1,1 @@
-This directory contains files that are only relevant for official release artifacts - for example, instructions on how to get the binary working on certain Linux distros.
+This directory contains files that are only relevant for official release artifacts.
--- scripts/build_nixpkgs_local.nix
+++ scripts/build_nixpkgs_local.nix
@@ -2,44 +2,15 @@
 
 let
   packageOverrides = pkgs: {
-    bambootracker = (pkgs.bambootracker.override {
-			# RtAudio in stable Nixpkgs still on 5.0.0
-      rtaudio = pkgs.rtaudio.overrideAttrs (oa: rec {
-        version = "6.0.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "thestk";
-          repo = "rtaudio";
-          rev = version;
-          hash = "sha256-/pbtezm3Ceo9WsMI1REJ9y72aZck55tRzrsifNeKrXA=";
-        };
-      });
-    }).overrideAttrs (oldAttrs: {
+    bambootracker = pkgs.bambootracker.overrideAttrs (oldAttrs: {
       version = buildVersion;
       src = ./..;
-      patches = [];
-      preConfigure = "lupdate Project.pro";
+      patches = [ ];
+      preConfigure = (oldAttrs.preConfigure or "") + ''
+        lupdate Project.pro
+      '';
     });
   };
   pkgs = import <nixpkgs> { config = { inherit packageOverrides; }; };
-  nix-bundle-patched = pkgs.nix-bundle.overrideAttrs (oa: {
-    # https://github.com/matthewbauer/nix-bundle/pull/98
-    patches = (oa.patches or []) ++ [
-      (pkgs.fetchpatch {
-        name = "0001-nix-bundle-nix-user-chroot-Link-libgcc_s.so.1.patch";
-        url = "https://github.com/matthewbauer/nix-bundle/pull/98/commits/c047b32dc4d004189c3564eae458aecc5d28f25d.patch";
-        hash = "sha256-VR45xP7rw3GzIrkznHLPPf9OIVZnlnezUjCKhfQJ47s=";
-      })
-    ];
-    # https://github.com/matthewbauer/nix-bundle/issues/72
-    postInstall = (oa.postInstall or "") + ''
-      substituteInPlace $out/share/nix-bundle/nix-user-chroot/Makefile \
-        --replace 'g++' 'g++ -static-libstdc++'
-    '';
-  });
-in {
-  build = pkgs.bambootracker;
-  bundle = (import "${nix-bundle-patched}/share/nix-bundle/default.nix" {}).nix-bootstrap {
-    target = pkgs.bambootracker;
-    run = "/bin/BambooTracker";
-  };
-}
+in
+pkgs.bambootracker-qt6
